57bdf22b4974399706d76fcb860323a78d3710ea
Roscoe Kerby <56516234+roscoekerby@users.noreply.github.com>
2022-12-12 20:21:26 +0200

    Made nicer :)

--- rename_files_recursively.sh
+++ rename_files_recursively.sh
@@ -1,45 +1,36 @@
 counter=0
 folder_name="folder_name"
 
-print_folder_recurse() {
-	
-	
-    for i in "$1"/*;do
-        if [ -d "$i" ];then
-            echo "dir: $i"
-            folder_name=$i            
-            print_folder_recurse "$i"
-        elif [ -f "$i" ]; then
-        	counter=$((counter+1))
-        	echo "$folder_name"
-        	variablename='\usr\bin\env '
-			myvariable='_Awesome'
+iterate_folders_recursively() {	
+    for item in "$1"/*;do
+        if [ -d "$item" ];then
+            echo "dir: $item"
+            folder_name=$item            
+            iterate_folders_recursively "$item"
+        elif [ -f "$item" ]; then
+			counter=$((counter+1))
 			varname="$folder_name"_"$counter"
-			echo "$varname"
-			SUBSTRING=$(echo $folder_name| cut -d'/' -f 2)
-			echo $SUBSTRING
-			SUBSTRING2=$(echo $varname| cut -d'/' -f 2)
-			echo $SUBSTRING2
-			SUBSTRING3=$(echo $varname| cut -d'/' -f 1)
-			echo $SUBSTRING3
-			final="$SUBSTRING3/"
-			final+="$SUBSTRING/" 
-			final+="$SUBSTRING2"
-			echo "$final"
-        	mv "$i" "$final"
-           	echo "renamed file: "$i" to "$SUBSTRING2""
-            
+			class_name=$(echo $folder_name| cut -d'/' -f 2)
+			new_file_name=$(echo $varname| cut -d'/' -f 2)
+			root_dir=$(echo $varname| cut -d'/' -f 1)
+			final="$root_dir/"
+			final+="$class_name/" 
+			final+="$new_file_name"
+			final+=".jpg"
+			mv "$item" "$final"
+		   	echo "renamed file: "$item" to "$new_file_name".jpg"
         fi
     done
 }
 
 # try get path from param
 path="test"
+
 if [ -d "$1" ]; then
     path=$1;
 else
     path="test"
 fi
 
-#echo "base path: $path"
-print_folder_recurse $path
+# echo "base path: $path"
+iterate_folders_recursively $path
